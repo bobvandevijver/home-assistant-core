8f25d2b41ecd0cc83780e3e6f88df5317f35f356
Bob van de Vijver <bobvandevijver@hotmail.com>
2025-02-02 14:47:22 +0000

    [Shelly] Add support for test events

--- homeassistant/components/shelly/const.py
+++ homeassistant/components/shelly/const.py
@@ -116,10 +116,15 @@ BATTERY_DEVICES_WITH_PERMANENT_CONNECTION: Final = [
 # Button/Click events for Block & RPC devices
 EVENT_SHELLY_CLICK: Final = "shelly.click"
 
+# Test events for RPC devices
+EVENT_SHELLY_TEST: Final = "shelly.test"
+
 ATTR_CLICK_TYPE: Final = "click_type"
 ATTR_CHANNEL: Final = "channel"
+ATTR_COMPONENT: Final = "component"
 ATTR_DEVICE: Final = "device"
 ATTR_GENERATION: Final = "generation"
+ATTR_TEST_TYPE: Final = "test_type"
 CONF_SUBTYPE: Final = "subtype"
 ATTR_BETA: Final = "beta"
 CONF_OTA_BETA_CHANNEL: Final = "ota_beta_channel"
@@ -135,6 +140,10 @@ RPC_INPUTS_EVENTS_TYPES: Final = {
     "double_push",
     "triple_push",
     "long_push",
+}
+
+RPC_TEST_EVENTS_TYPES: Final = {
+    "alarm_test",
 }
 
 BLOCK_INPUTS_EVENTS_TYPES: Final = {
